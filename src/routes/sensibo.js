10e99dae7cbc7f48e76043b1fc3cbd7b5fdc1c7c
Lauri Mäkinen <lauri@mits.fi>
2019-07-20 13:46:27 +0300

    Reoder rooms so that the rooms are in logical order * "Yläkerta" before / above "Alakerta"

--- src/routes/sensibo.js
+++ src/routes/sensibo.js
@@ -3,6 +3,14 @@ const httpClient = require("../utils/httpClient")
 
 const api = require("../utils/api")
 
+const arrayMove = (arr, fromIndex, toIndex) => {
+  const element = arr.result[fromIndex];
+  arr.result.splice(fromIndex, 1);
+  arr.result.splice(toIndex, 0, element);
+
+  return arr;
+}
+
 const sensiboRouter = express.Router()
 sensiboRouter
   .get("/devices", async (_, res) => {
@@ -12,7 +20,9 @@ sensiboRouter
       { params: qsObject }
     )
 
-    return res.status(200).json(activeDevices)
+    const reOrderedActiveDevices = arrayMove(activeDevices, 0, 1);
+
+    return res.status(200).json(reOrderedActiveDevices)
   })
 
 module.exports = sensiboRouter
