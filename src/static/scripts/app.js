a1f944619f5bdde5b5860b0b4f96fa5cf6adb059
Lauri Mäkinen <lauri@mits.fi>
2019-07-27 17:20:23 +0300

    Display line charts based on gathered upstairs & downstairs data

--- src/static/scripts/app.js
+++ src/static/scripts/app.js
@@ -1,4 +1,4 @@
-Vue.component('device-template', {
+Vue.component("device-template", {
   name: "device-template",
   props: ["device"],
   computed: {
@@ -37,16 +37,32 @@ Vue.component('device-template', {
   `
 })
 
+Vue.component("LineChart", {
+  name: "line-chart",
+  extends: VueChartJs.Line,
+  props: ["labels", "datasets"],
+  mounted () {
+    this.renderChart(
+      { labels: this.labels, datasets: this.datasets },
+      { responsive: true, maintainAspectRatio: false }
+    )
+  }
+})
+
 new Vue({
   el: "#vue-root",
-  data() {
-    return {
-      devicesLoading: false,
-      activeDevices: [],
-      fetchingInterval: undefined,
-    }
-  },
+  data: () => ({
+    devicesLoading: false,
+    upstairsDataLoaded: false,
+    downstairsDataLoaded: false,
+    activeDevices: [],
+    fetchingInterval: undefined,
+    upstairsData: [],
+    downstairsData: [],
+  }),
   async mounted() {
+    this.getUpstairsData()
+    this.getDownstairsData()
     this.activeDevices = await this.getSensiboData()
 
     this.fetchingInterval = setInterval(async () => {
@@ -57,6 +73,44 @@ new Vue({
     clearInterval(this.fetchingInterval)
   },
   methods: {
+    getUpstairsData: async function() {
+      const res = await fetch("/api/v0/sensibo/status/wUpzsX6u")
+      const json = await res.json()
+
+      const data = [
+        {
+          label: "Lämpötila",
+          backgroundColor: "#9bdbff",
+          data: json.map(p => p.temperature),
+        }, {
+          label: "Kosteus",
+          backgroundColor: "#ffaf66",
+          data: json.map(p => p.humidity),
+        }
+      ]
+
+      this.upstairsData = data
+      this.upstairsDataLoaded = true
+    },
+    getDownstairsData: async function() {
+      const res = await fetch("/api/v0/sensibo/status/GGJKvCDD")
+      const json = await res.json()
+
+      const data = [
+        {
+          label: "Lämpötila",
+          backgroundColor: "#9bdbff",
+          data: json.map(p => p.temperature),
+        }, {
+          label: "Kosteus",
+          backgroundColor: "#ffaf66",
+          data: json.map(p => p.humidity),
+        }
+      ]
+
+      this.downstairsData = data
+      this.downstairsDataLoaded = true
+    },
     getSensiboData: async () => {
       this.devicesLoading = true
 
