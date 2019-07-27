304c4e3801717ea78c9df6c60d9c1687a4bbd594
Lauri Mäkinen <lauri@mits.fi>
2019-07-27 15:59:33 +0300

    Save device status to DB every 5 mnutes instead of 1

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -4,8 +4,8 @@ const db = require("../utils/db")
 const api = require("../utils/api")
 
 const setupCronJobs = () => {
-  // https://crontab.guru/every-1-minute
-  new CronJob("* * * * *", async () => {
+  // https://crontab.guru/every-5-minutes
+  new CronJob("*/5 * * * *", async () => {
     console.log("CRON: Fetching device statuses & saving to DB...") // eslint-disable-line
 
     const allDevices = await api.getActiveDevices()
